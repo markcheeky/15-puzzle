45e71bd26d92346cae732431530762c04454a5a1
makadlcik <makadlcik@gmail.com>
2015-08-29 21:55:31 +0200

    style improvement

--- interaction.js
+++ interaction.js
@@ -1,29 +1,32 @@
-function moveEvent(direction) {
-	//alert(direction); //for testing purposes only
+function moveEvent(e) {
+	//alert(e.which); //for testing purposes only
 	/ directions: left = 37, up = 38, right = 39, down = 40 /
-    switch(direction) {
+    switch(e.which) {
         case 37:
             move("left", true);
+			e.preventDefault();
             break;
             
         case 38:
             move("up", true);
+			e.preventDefault();
             break;
             
         case 39:
             move("right", true);
+			e.preventDefault();
             break;
             
         case 40:
             move("down", true);
+			e.preventDefault();
             break;
     }
 }
 
 $(function(){
 	$('html').keydown(function(e) {
-		moveEvent(e.which);
-		e.preventDefault();
+		moveEvent(e);
 	});
 });
 
