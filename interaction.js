662805c3668526e14536f450362b19ac37d252c8
makadlcik <makadlcik@gmail.com>
2015-10-03 21:35:26 +0200

    remove jquery mobile -> use hammer.js again

--- interaction.js
+++ interaction.js
@@ -35,117 +35,27 @@ $(function(){
 });
 
 // game field catches user swipe actions
-// $(function hammerListener() {
-// 	var options = {
-// 		touchAction: "pan-x"
-// 	};
-// 	var mc = new Hammer(document.getElementById('game-field'), options);
-// 	mc.get('swipe').set({ direction: Hammer.DIRECTION_ALL});
+$(function hammerListener() {
+	var options = {
+		touchAction: "pan-x"
+	};
+	var mc = new Hammer(document.getElementById('game-field'), options);
+	mc.get('swipe').set({ direction: Hammer.DIRECTION_ALL, velocity:0.5});
 
-// 	mc.on("swipeleft", function(ev) {
-// 		move("left", true);
-// 	});
-
-// 	mc.on("swiperight", function(ev) {
-// 		move("right", true);
-// 	});	
-
-// 	mc.on("swipeup", function(ev) {
-// 		move("up", true);
-// 	});	
-
-// 	mc.on("swipedown", function(ev) {
-// 		move("down", true);
-// 	});	
-
-// });
-
-var supportTouch = $.support.touch,
-            scrollEvent = "touchmove scroll",
-            touchStartEvent = supportTouch ? "touchstart" : "mousedown",
-            touchStopEvent = supportTouch ? "touchend" : "mouseup",
-            touchMoveEvent = supportTouch ? "touchmove" : "mousemove";
-    $.event.special.swipeupdown = {
-        setup: function() {
-            var thisObject = this;
-            var $this = $(thisObject);
-            $this.bind(touchStartEvent, function(event) {
-                var data = event.originalEvent.touches ?
-                        event.originalEvent.touches[ 0 ] :
-                        event,
-                        start = {
-                            time: (new Date).getTime(),
-                            coords: [ data.pageX, data.pageY ],
-                            origin: $(event.target)
-                        },
-                        stop;
-
-                function moveHandler(event) {
-                    if (!start) {
-                        return;
-                    }
-                    var data = event.originalEvent.touches ?
-                            event.originalEvent.touches[ 0 ] :
-                            event;
-                    stop = {
-                        time: (new Date).getTime(),
-                        coords: [ data.pageX, data.pageY ]
-                    };
-
-                    // prevent scrolling
-                    if (Math.abs(start.coords[1] - stop.coords[1]) > 10) {
-                        event.preventDefault();
-                    }
-                }
-                $this
-                        .bind(touchMoveEvent, moveHandler)
-                        .one(touchStopEvent, function(event) {
-                    $this.unbind(touchMoveEvent, moveHandler);
-                    if (start && stop) {
-                        if (stop.time - start.time < 1000 &&
-                                Math.abs(start.coords[1] - stop.coords[1]) > 30 &&
-                                Math.abs(start.coords[0] - stop.coords[0]) < 75) {
-                            start.origin
-                                    .trigger("swipeupdown")
-                                    .trigger(start.coords[1] > stop.coords[1] ? "swipeup" : "swipedown");
-                        }
-                    }
-                    start = stop = undefined;
-                });
-            });
-        }
-    };
-    $.each({
-        swipedown: "swipeupdown",
-        swipeup: "swipeupdown"
-    }, function(event, sourceEvent){
-        $.event.special[event] = {
-            setup: function(){
-                $(this).bind(sourceEvent, $.noop);
-            }
-        };
-    });
-
-
-$(function touchListener() {
-	$("#game-field").on( "swipeleft", function(event) {
- 		event.stopPropagation();
- 		event.preventDefault();
+	mc.on("swipeleft", function(ev) {
 		move("left", true);
 	});
-	$("#game-field").on( "swiperight", function(event) {
- 		event.stopPropagation();
- 		event.preventDefault();
- 		move("right", true);
-	});
-	$("#game-field").on( "swipeup", function(event) {
- 		event.stopPropagation();
- 		event.preventDefault();
- 		move("up", true);
-	});
-	$("#game-field").on( "swipedown", function(event) {
- 		event.stopPropagation();
- 		event.preventDefault();
- 		move("down", true);
-	});
+
+	mc.on("swiperight", function(ev) {
+		move("right", true);
+	});	
+
+	mc.on("swipeup", function(ev) {
+		move("up", true);
+	});	
+
+	mc.on("swipedown", function(ev) {
+		move("down", true);
+	});	
+
 });
